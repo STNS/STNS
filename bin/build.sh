add4ec489ea67f26018302ba96f164f9335837a3
kazuhiko yamahsita <pyama@pepabo.com>
2016-01-07 22:12:13 +0900

    tmp commit

--- bin/build.sh
+++ bin/build.sh
@@ -1,2 +1,2 @@
 eval $(docker-machine env dev)
-docker build -t centos:stns . && docker run -v "$(pwd)"/releases:/go/src/github.com/pyama86/STNS/RPM/RPMS centos:stns
+docker build -t centos:stns . && docker run -v "$(pwd)"/releases:/go/src/github.com/pyama86/STNS/package/RPM/RPMS centos:stns
