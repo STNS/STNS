dc245d922eaeda0e9136d4b31f8fdd1fa848c2c9
pyama86 <pyama@pepabo.com>
2016-01-10 00:00:54 +0900

    rename docker file

--- bin/ubuntu_build.sh
+++ bin/ubuntu_build.sh
@@ -1,3 +1,3 @@
 eval $(docker-machine env dev)
 rm -rf releases/stns*deb
-docker build -f DebDockerfile -t ubuntu:stns . && docker run -v "$(pwd)"/releases:/go/src/github.com/pyama86/STNS/releases ubuntu:stns
+docker build -f UbuntuDockerfile -t ubuntu:stns . && docker run -v "$(pwd)"/releases:/go/src/github.com/pyama86/STNS/releases ubuntu:stns
