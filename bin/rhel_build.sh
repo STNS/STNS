bd83df5865a207fe2b1f618f2d21bc6faf8efc98
kazuhiko yamahsita <pyama@pepabo.com>
2016-01-10 11:15:23 +0900

    move docker file

--- bin/rhel_build.sh
+++ bin/rhel_build.sh
@@ -1,3 +1,3 @@
 eval $(docker-machine env dev)
 rm -rf releases/stns*rpm
-docker build -f RhelDockerfile -t centos:stns . && docker run -v "$(pwd)"/releases:/go/src/github.com/pyama86/STNS/releases centos:stns
+docker build -f docker/Rhel -t centos:stns . && docker run -v "$(pwd)"/releases:/go/src/github.com/pyama86/STNS/releases centos:stns
--- bin/ubuntu_build.sh
+++ bin/ubuntu_build.sh
@@ -1,3 +1,3 @@
 eval $(docker-machine env dev)
 rm -rf releases/stns*deb
-docker build -f UbuntuDockerfile -t ubuntu:stns . && docker run -v "$(pwd)"/releases:/go/src/github.com/pyama86/STNS/releases ubuntu:stns
+docker build -f docker/Ubuntu -t ubuntu:stns . && docker run -v "$(pwd)"/releases:/go/src/github.com/pyama86/STNS/releases ubuntu:stns
