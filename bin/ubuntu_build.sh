2d00e27c7848b78edefaf0e8ca5c098f796f4a22
kazuhiko yamahsita <pyama@pepabo.com>
2016-01-10 20:59:25 +0900

    add rm

--- bin/ubuntu_build.sh
+++ bin/ubuntu_build.sh
@@ -1,3 +1,3 @@
 eval $(docker-machine env dev)
 rm -rf releases/stns*deb
-docker build -f docker/Ubuntu -t ubuntu:stns . && docker run -v "$(pwd)"/releases:/go/src/github.com/pyama86/STNS/releases ubuntu:stns
+docker build --rm -f docker/Ubuntu -t ubuntu:stns . && docker run -v "$(pwd)"/releases:/go/src/github.com/pyama86/STNS/releases ubuntu:stns
