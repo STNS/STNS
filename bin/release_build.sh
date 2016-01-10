4b71f052b4174942fa9901da1f806427e24dc7f1
kazuhiko yamahsita <pyama@pepabo.com>
2016-01-10 21:31:16 +0900

    rename

--- bin/release_build.sh
+++ bin/release_build.sh
@@ -1,3 +1,1 @@
-eval $(docker-machine env dev)
-rm -rf releases/stns*deb
-docker build --rm -f docker/Ubuntu -t ubuntu:stns . && docker run -v "$(pwd)"/releases:/go/src/github.com/pyama86/STNS/releases ubuntu:stns
+bin/rhel_build.sh && bin/ubuntu_build.sh
---
+++ bin/ubuntu_build.sh
@@ -0,0 +1,3 @@
+eval $(docker-machine env dev)
+rm -rf releases/stns*deb
+docker build --rm -f docker/Ubuntu -t ubuntu:stns . && docker run -v "$(pwd)"/releases:/go/src/github.com/pyama86/STNS/releases ubuntu:stns
