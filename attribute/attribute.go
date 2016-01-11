5151d1a24961698c4b35c299a83e5f8dfcf809d8
kazuhiko yamahsita <pyama@pepabo.com>
2016-01-12 04:02:39 +0900

    delete user struct

--- attribute/attribute.go
+++ attribute/attribute.go
@@ -1,16 +1,13 @@
 package attribute
 
-type User struct {
+type All struct {
+	Id int `toml:"id" json:"id"`
+	// use user
 	GroupId   int      `toml:"group_id" json:"group_id"`
 	Directory string   `toml:"directory" json:"directory"`
 	Shell     string   `toml:"shell" json:"shell"`
 	Gecos     string   `toml:"gecos" json:"gecos"`
 	Keys      []string `toml:"keys" json:"keys"`
-}
-
-type All struct {
-	Id int `toml:"id" json:"id"`
-	*User
 	// use group
 	Users []string `toml:"users" json:"users"`
 }
