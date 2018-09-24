337a8ca94259b627df4f8ceb0d229d2a839be81d
pyama86 <pyama@pepabo.com>
2018-09-24 12:37:43 +0900

    最大、最初のIDがわからないケースも有る

--- middleware/add_header.go
+++ middleware/add_header.go
@@ -12,11 +12,15 @@ func AddHeader(backend model.Backend) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return echo.HandlerFunc(func(c echo.Context) error {
 			if strings.Index(c.Path(), "/users") > 0 {
-				c.Response().Header().Add("USER-HIGHEST-ID", strconv.Itoa(backend.HighestUserID()))
-				c.Response().Header().Add("USER-LOWEST-ID", strconv.Itoa(backend.LowestUserID()))
+				if backend.HighestUserID() != 0 && backend.LowestUserID() != 0 {
+					c.Response().Header().Add("USER-HIGHEST-ID", strconv.Itoa(backend.HighestUserID()))
+					c.Response().Header().Add("USER-LOWEST-ID", strconv.Itoa(backend.LowestUserID()))
+				}
 			} else if strings.Index(c.Path(), "/groups") > 0 {
-				c.Response().Header().Add("GROUP-HIGHEST-ID", strconv.Itoa(backend.HighestGroupID()))
-				c.Response().Header().Add("GROUP-LOWEST-ID", strconv.Itoa(backend.LowestGroupID()))
+				if backend.HighestGroupID() != 0 && backend.LowestGroupID() != 0 {
+					c.Response().Header().Add("GROUP-HIGHEST-ID", strconv.Itoa(backend.HighestGroupID()))
+					c.Response().Header().Add("GROUP-LOWEST-ID", strconv.Itoa(backend.LowestGroupID()))
+				}
 			}
 			if err := next(c); err != nil {
 				return err
