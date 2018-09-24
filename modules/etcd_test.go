d5a0097706a00d3b53233278f036451ae199add1
pyama86 <pyama@pepabo.com>
2018-09-25 06:57:03 +0900

    最大ID、最小IDを取れるようにした

--- modules/etcd_test.go
+++ modules/etcd_test.go
@@ -1,6 +1,7 @@
 package modules
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 
@@ -172,13 +173,92 @@ func TestBackendEtcd_FindUserByName(t *testing.T) {
 	}
 }
 
-func TestBackendEtcd_FindUsers(t *testing.T) {
+func TestBackendEtcd_Users(t *testing.T) {
 	type fields struct {
 		config *stns.Config
 	}
 	tests := []struct {
 		name    string
 		fields  fields
+		params  model.Users
+		want    map[string]model.UserGroup
+		wantErr bool
+	}{
+		{
+			name: "ok",
+			fields: fields{
+				config: &stns.Config{
+					Modules: map[string]interface{}{
+						"etcd": map[string]interface{}{
+							"endpoints": []string{"http://127.0.0.1:2379"},
+						},
+					},
+				},
+			},
+			params: model.Users{
+				"user1": &model.User{
+					Base: model.Base{
+						ID:   1,
+						Name: "user1",
+					},
+				},
+				"user10": &model.User{
+					Base: model.Base{
+						ID:   10,
+						Name: "user10",
+					},
+				},
+			},
+			want: map[string]model.UserGroup{
+				"user1": &model.User{
+					Base: model.Base{
+						ID:   1,
+						Name: "user1",
+					},
+				},
+				"user10": &model.User{
+					Base: model.Base{
+						ID:   10,
+						Name: "user10",
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			b, err := NewBackendEtcd(tt.fields.config)
+			if err != nil {
+				t.Fatal(err)
+			}
+			for _, v := range tt.params {
+				if err := b.Create(fmt.Sprintf("/users/id/%d", v.ID), v); err != nil {
+					t.Fatal(err)
+				}
+			}
+			got, err := b.Users()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("BackendEtcd.Users() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("BackendEtcd.Users() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestBackendEtcd_FindGroupByID(t *testing.T) {
+	type fields struct {
+		config *stns.Config
+	}
+	type args struct {
+		id int
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
 		params  model.UserGroup
 		want    map[string]model.UserGroup
 		wantErr bool
@@ -194,17 +274,191 @@ func TestBackendEtcd_FindUsers(t *testing.T) {
 					},
 				},
 			},
-			params: &model.User{
+			args: args{
+				id: 1,
+			},
+			params: &model.Group{
 				Base: model.Base{
 					ID:   1,
-					Name: "user1",
+					Name: "group1",
 				},
 			},
 			want: map[string]model.UserGroup{
-				"user1": &model.User{
+				"group1": &model.Group{
 					Base: model.Base{
 						ID:   1,
-						Name: "user1",
+						Name: "group1",
+					},
+				},
+			},
+		},
+		{
+			name: "notfound",
+			fields: fields{
+				config: &stns.Config{
+					Modules: map[string]interface{}{
+						"etcd": map[string]interface{}{
+							"endpoints": []string{"http://127.0.0.1:2379"},
+						},
+					},
+				},
+			},
+			args: args{
+				id: 99999,
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			b, err := NewBackendEtcd(tt.fields.config)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if err := b.Create("/groups/id/1", tt.params); err != nil {
+				t.Fatal(err)
+			}
+			got, err := b.FindGroupByID(tt.args.id)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("BackendEtcd.FindGroupByID() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("BackendEtcd.FindGroupByID() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestBackendEtcd_FindGroupByName(t *testing.T) {
+	type fields struct {
+		config *stns.Config
+	}
+	type args struct {
+		name string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		params  model.UserGroup
+		want    map[string]model.UserGroup
+		wantErr bool
+	}{
+		{
+			name: "ok",
+			fields: fields{
+				config: &stns.Config{
+					Modules: map[string]interface{}{
+						"etcd": map[string]interface{}{
+							"endpoints": []string{"http://127.0.0.1:2379"},
+						},
+					},
+				},
+			},
+			args: args{
+				name: "group1",
+			},
+			params: &model.Group{
+				Base: model.Base{
+					ID:   1,
+					Name: "group1",
+				},
+			},
+			want: map[string]model.UserGroup{
+				"group1": &model.Group{
+					Base: model.Base{
+						ID:   1,
+						Name: "group1",
+					},
+				},
+			},
+		},
+		{
+			name: "notfound",
+			fields: fields{
+				config: &stns.Config{
+					Modules: map[string]interface{}{
+						"etcd": map[string]interface{}{
+							"endpoints": []string{"http://127.0.0.1:2379"},
+						},
+					},
+				},
+			},
+			args: args{
+				name: "notfound",
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			b, err := NewBackendEtcd(tt.fields.config)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if err := b.Create("/groups/name/group1", tt.params); err != nil {
+				t.Fatal(err)
+			}
+			got, err := b.FindGroupByName(tt.args.name)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("BackendEtcd.FindGroupByName() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("BackendEtcd.FindGroupByName() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestBackendEtcd_Groups(t *testing.T) {
+	type fields struct {
+		config *stns.Config
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		params  model.Groups
+		want    map[string]model.UserGroup
+		wantErr bool
+	}{
+		{
+			name: "ok",
+			fields: fields{
+				config: &stns.Config{
+					Modules: map[string]interface{}{
+						"etcd": map[string]interface{}{
+							"endpoints": []string{"http://127.0.0.1:2379"},
+						},
+					},
+				},
+			},
+			params: model.Groups{
+				"group1": &model.Group{
+					Base: model.Base{
+						ID:   1,
+						Name: "group1",
+					},
+				},
+				"group10": &model.Group{
+					Base: model.Base{
+						ID:   10,
+						Name: "group10",
+					},
+				},
+			},
+			want: map[string]model.UserGroup{
+				"group1": &model.Group{
+					Base: model.Base{
+						ID:   1,
+						Name: "group1",
+					},
+				},
+				"group10": &model.Group{
+					Base: model.Base{
+						ID:   10,
+						Name: "group10",
 					},
 				},
 			},
@@ -216,16 +470,209 @@ func TestBackendEtcd_FindUsers(t *testing.T) {
 			if err != nil {
 				t.Fatal(err)
 			}
-			if err := b.Create("/users/id/1", tt.params); err != nil {
-				t.Fatal(err)
+			for _, v := range tt.params {
+				if err := b.Create(fmt.Sprintf("/groups/id/%d", v.ID), v); err != nil {
+					t.Fatal(err)
+				}
 			}
-			got, err := b.Users()
+
+			got, err := b.Groups()
 			if (err != nil) != tt.wantErr {
-				t.Errorf("BackendEtcd.Users() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("BackendEtcd.Groups() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("BackendEtcd.Users() = %v, want %v", got, tt.want)
+				t.Errorf("BackendEtcd.Groups() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestBackendEtcd_highlowUserID(t *testing.T) {
+	type fields struct {
+		config *stns.Config
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		params  model.Users
+		want    int
+		wantErr bool
+		high    bool
+	}{
+		{
+			name: "high",
+			fields: fields{
+				config: &stns.Config{
+					Modules: map[string]interface{}{
+						"etcd": map[string]interface{}{
+							"endpoints": []string{"http://127.0.0.1:2379"},
+						},
+					},
+				},
+			},
+			params: model.Users{
+				"user1": &model.User{
+					Base: model.Base{
+						ID:   1,
+						Name: "user1",
+					},
+				},
+				"user10": &model.User{
+					Base: model.Base{
+						ID:   10,
+						Name: "user10",
+					},
+				},
+			},
+			want: 10,
+			high: true,
+		},
+		{
+			name: "low",
+			fields: fields{
+				config: &stns.Config{
+					Modules: map[string]interface{}{
+						"etcd": map[string]interface{}{
+							"endpoints": []string{"http://127.0.0.1:2379"},
+						},
+					},
+				},
+			},
+			params: model.Users{
+				"user1": &model.User{
+					Base: model.Base{
+						ID:   1,
+						Name: "user1",
+					},
+				},
+				"user10": &model.User{
+					Base: model.Base{
+						ID:   10,
+						Name: "user10",
+					},
+				},
+			},
+			want: 1,
+			high: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_b, err := NewBackendEtcd(tt.fields.config)
+			if err != nil {
+				t.Fatal(err)
+			}
+			b := _b.(BackendEtcd)
+
+			for _, v := range tt.params {
+				if err := b.Create(fmt.Sprintf("/users/id/%d", v.ID), v); err != nil {
+					t.Fatal(err)
+				}
+			}
+
+			got := b.highlowUserID(tt.high)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("BackendEtcd.highlowUserID() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("BackendEtcd.highlowUserID() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestBackendEtcd_highlowGroupID(t *testing.T) {
+	type fields struct {
+		config *stns.Config
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		params  model.Groups
+		want    int
+		wantErr bool
+		high    bool
+	}{
+		{
+			name: "high",
+			fields: fields{
+				config: &stns.Config{
+					Modules: map[string]interface{}{
+						"etcd": map[string]interface{}{
+							"endpoints": []string{"http://127.0.0.1:2379"},
+						},
+					},
+				},
+			},
+			params: model.Groups{
+				"group1": &model.Group{
+					Base: model.Base{
+						ID:   1,
+						Name: "group1",
+					},
+				},
+				"group10": &model.Group{
+					Base: model.Base{
+						ID:   10,
+						Name: "group10",
+					},
+				},
+			},
+			want: 10,
+			high: true,
+		},
+		{
+			name: "low",
+			fields: fields{
+				config: &stns.Config{
+					Modules: map[string]interface{}{
+						"etcd": map[string]interface{}{
+							"endpoints": []string{"http://127.0.0.1:2379"},
+						},
+					},
+				},
+			},
+			params: model.Groups{
+				"group1": &model.Group{
+					Base: model.Base{
+						ID:   1,
+						Name: "group1",
+					},
+				},
+				"group10": &model.Group{
+					Base: model.Base{
+						ID:   10,
+						Name: "group10",
+					},
+				},
+			},
+			want: 1,
+			high: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_b, err := NewBackendEtcd(tt.fields.config)
+			if err != nil {
+				t.Fatal(err)
+			}
+			b := _b.(BackendEtcd)
+
+			for _, v := range tt.params {
+				if err := b.Create(fmt.Sprintf("/groups/id/%d", v.ID), v); err != nil {
+					t.Fatal(err)
+				}
+			}
+
+			got := b.highlowGroupID(tt.high)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("BackendEtcd.highlowGroupID() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("BackendEtcd.highlowGroupID() = %v, want %v", got, tt.want)
 			}
 		})
 	}
