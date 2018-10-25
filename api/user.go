a7e9003bbbbebca914f96a153db35780ae1ff986
pyama86 <pyama@pepabo.com>
2018-10-26 01:04:38 +0900

    パスワード更新できるようにしたにょりー

--- api/user.go
+++ api/user.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
 
@@ -45,6 +46,43 @@ func getUsers(c echo.Context) error {
 	return c.JSON(http.StatusOK, toSlice(r))
 }
 
+func updateUserPassword(c echo.Context) error {
+	backend := c.Get(middleware.BackendKey).(model.Backend)
+	u := struct {
+		CurrentPassword string
+		NewPassword     string
+	}{}
+
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+
+	if err := c.Bind(&u); err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+
+	r, err := backend.FindUserByID(id)
+	if err != nil {
+		return errorResponse(c, err)
+	}
+
+	for _, us := range r {
+		user := us.(*model.User)
+		if user.Password == u.CurrentPassword {
+			user.Password = u.CurrentPassword
+
+			err := backend.Update(fmt.Sprintf("/users/name/%s", user.Name), user)
+			if err != nil {
+				return errorResponse(c, err)
+			}
+			return c.JSON(http.StatusOK, user)
+		}
+	}
+	return c.JSON(http.StatusBadRequest, nil)
+}
+
 func UserEndpoints(g *echo.Group) {
 	g.GET("/users", getUsers)
+	g.PUT("/users/password/:id", updateUserPassword)
 }
