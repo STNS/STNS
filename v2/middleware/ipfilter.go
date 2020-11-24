5082a8d682dd81fc77c04cbaf632053fa06988e3
pyama86 <pyama@pepabo.com>
2020-11-24 18:20:40 +0900

    use echo api

--- v2/middleware/ipfilter.go
+++ v2/middleware/ipfilter.go
@@ -1,8 +1,6 @@
 package middleware
 
 import (
-	"net"
-
 	"github.com/jpillora/ipfilter"
 	"github.com/labstack/echo"
 	"github.com/labstack/gommon/log"
@@ -26,11 +24,10 @@ func IPFilterWithConfig(config IPFilterConfig) echo.MiddlewareFunc {
 				AllowedIPs:     config.AllowIPs,
 				BlockByDefault: true,
 			})
-			ip, _, _ := net.SplitHostPort(c.Request().RemoteAddr)
 
-			if config.AllowIPs != nil && !f.Allowed(ip) {
+			if config.AllowIPs != nil && !f.Allowed(c.RealIP()) {
 				if config.Logger != nil {
-					config.Logger.Debugf("access denied %s", c.Request().RemoteAddr)
+					config.Logger.Infof("access denied %s", c.RealIP())
 				}
 				return echo.ErrUnauthorized
 			}
